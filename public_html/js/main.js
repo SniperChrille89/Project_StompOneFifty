4e3777ba5e8adbe65106f674b6b8e28e9d3dc396
SniperChrille89 <olsson.c.max@gmail.com>
2018-10-15 10:13:31 +0200

    Javascript works!
    Javascript linked to the form works like a charm!

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -13,29 +13,50 @@ function navToggle() {
     else {
 
         x.className = "navigationBar";
+
     }
 
 }
 
 
+
 // POP MESSAGE WINDOW
 
-
-
 var popWindow = document.getElementById('popUp');
-
 var button = document.getElementById('button');
-
 var span = document.getElementsByClassName('close')[0];
+var form = document.getElementById('form');
+var inputs = form.getElementsByTagName('input');
 
-button.onclick = function() {
+
+button.onclick = function(event) {
+
+    for (var i = 0; i < inputs.length; i++) {
+
+        if (inputs[i].hasAttribute("required")) {
+
+            if (inputs[i].value == "") {
+
+                alert("Please fill all the fields!");
+                return false;
+
+            }
+        }
+    }
 
     popWindow.style.display = "block";
+    console.log("SHOW");
+    event.preventDefault();
 
 }
 
 span.onclick = function() {
 
-    popWindow.style.display = "hidden";
+    popWindow.style.display = "none";
+    console.log("HIDE");
+    form.submit();
 
 }
+
+
+
